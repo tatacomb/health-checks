412b96e17e34056123f574c3664cc65f3a5435c6
tatacomb <102047896+tatacomb@users.noreply.github.com>
2021-12-26 16:53:11 +0000

    Pontoon: Update Slovak (sk) localization of Mozilla Learning Network

--- stories/multiple.stories.tsx
+++ stories/multiple.stories.tsx
@@ -12,4 +12,41 @@ storiesOf('multiple', module)
         overflow: 'hidden',
       }}
     >
-      <Res
+      <Resizable
+        style={style}
+        defaultSize={{
+          width: '50%',
+          height: 200,
+        }}
+        maxWidth="100%"
+        minWidth="1"
+      >
+        001
+      </Resizable>
+      <div style={{ ...style, width: '100%', minWidth: '1px' }}>002</div>
+    </div>
+  ))
+  .add('vertical', () => (
+    <div
+      style={{
+        width: '300px',
+        height: '400px',
+        display: 'flex',
+        flexDirection: 'column',
+        overflow: 'hidden',
+      }}
+    >
+      <Resizable
+        style={style}
+        defaultSize={{
+          width: '300',
+          height: '200',
+        }}
+        maxHeight="398"
+        minHeight="1"
+      >
+        001
+      </Resizable>
+      <div style={{ ...style, width: '300px', height: '100%', minHeight: '1px' }}>002</div>
+    </div>
+  ));
---
+++ stories/vwvh.stories.tsx
@@ -0,0 +1,13 @@
+
+/* eslint-disable */
+
+import * as React from 'react';
+import { Resizable } from '../src';
+import { storiesOf } from '@storybook/react';
+import { style } from './style';
+
+storiesOf('vw vh', module)
+  .add('vw', () => (
+    <Resizable style={style} defaultSize={{ width: '50vw', height: '50vw' }}>
+      001
+    </Resizable>
